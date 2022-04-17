2165e215725a4d142a4acc3b2607f21d4f4686b3
Daniel Vršek <daniel.vrsek@gmail.com>
2022-04-17 10:48:17 +0200

    Replace delay with millis

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,11 +5,19 @@ dht DHT;
 #define DHT11_PIN 2
 #define DELAY 5000
 
+unsigned long timestamp = 0;
+
 void setup(){
   Serial.begin(9600);
 }
 
 void loop(){
+  unsigned long currentTimestamp = millis();
+  if (currentTimestamp - timestamp < DELAY){
+    return;
+  }
+  timestamp = currentTimestamp;
+  
   int chk = DHT.read11(DHT11_PIN);
   switch (chk)
   {
@@ -26,8 +34,6 @@ void loop(){
       printError(3); 
       return;
   }
-  
-  delay(DELAY);
 }
 
 void printData(){
