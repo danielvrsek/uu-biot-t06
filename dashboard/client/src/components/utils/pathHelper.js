c2cafd16a9c1548864f5812b2b4920c2e111be61
Jakub Pelz <jakubpelz@gmail.com>
2022-05-08 07:00:23 +0200

    Try Vercel

--- dashboard/client/src/components/utils/pathHelper.js
+++ dashboard/client/src/components/utils/pathHelper.js
@@ -1,3 +1,3 @@
 export const getBasePath = () => {
-  return 'http://localhost:4000';
+  return 'https://iotbe.herokuapp.com';
 };
--- dashboard/server/src/main.ts
+++ dashboard/server/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(4000);
+  await app.listen(PORT);
   /*  const app = await NestFactory.create(AppModule);
   await app.listen(4000); */
 }
