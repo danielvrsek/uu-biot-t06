556e2775fe8e41df8952c50ef1ae48cd553878ef
Daniel Vršek <daniel.vrsek@gmail.com>
2022-06-10 22:28:14 +0200

    Add granularity

--- dashboard/client/src/admin_console/components/chart/MainChartLoad.js
+++ dashboard/client/src/admin_console/components/chart/MainChartLoad.js
@@ -1,84 +1,83 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Error from '../core/Error';
 import Loading from '../core/Loading';
 import MainChartReady from './MainChartReady';
 
-import ApiClient from "../../../api/ApiClient";
+import ApiClient from '../../../api/ApiClient';
 
 const MainChartLoad = (props) => {
-//Chart
-const today = new Date();
-const yesterday = new Date(today);
-yesterday.setDate(yesterday.getDate() - 1);
-const defaultGranularity = 1800;
+    //Chart
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+    const defaultGranularity = 1800;
 
+    const [dateFrom, setDateFrom] = useState(yesterday);
+    const [dateTo, setDateTo] = useState(today);
+    const [granularity, setGranularity] = React.useState(defaultGranularity);
+    const [chartData, setChartData] = useState();
+    const [status, setStatus] = useState('loading');
 
-const [dateFrom, setDateFrom] = useState(yesterday);
-const [dateTo, setDateTo] = useState(today);
-const [granularity, setGranularity] = React.useState(defaultGranularity);
-const [chartData, setChartData] = useState();
-const [status, setStatus] = useState("loading");
+    const handleDateFrom = (newDateFrom) => {
+        setDateFrom(newDateFrom);
+    };
 
-const handleDateFrom = (newDateFrom) => {
-    setDateFrom(newDateFrom);
-};
+    const handleDateTo = (newDateTo) => {
+        setDateTo(newDateTo);
+    };
 
-const handleDateTo = (newDateTo) => {
-    setDateTo(newDateTo);
-};
+    const handleGranularity = (event) => {
+        setGranularity(event.target.value);
+    };
 
-const handleGranularity = (event) => {
-    setGranularity(event.target.value);
-};
+    const reset = () => {
+        setDateFrom(yesterday);
+        setDateTo(today);
+        setGranularity(defaultGranularity);
+    };
 
-const reset = () => {
-    setDateFrom(yesterday);
-    setDateTo(today);
-    setGranularity(defaultGranularity);
-};
+    const set = () => {
+        //call server
+    };
 
-const set = () => {
-//call server
-};
+    useEffect(() => {
+        ApiClient.getWeatherData(dateFrom, dateTo, granularity).then((res) => {
+            if (res.status === 200) {
+                setChartData(res.data);
+                setStatus('success');
+            } else {
+                setStatus('error');
+            }
+        });
+    }, [dateFrom, dateTo, granularity]);
 
-useEffect(() => {
-    ApiClient.getWeatherData(dateFrom.toISOString(), dateTo.toISOString()).then((res) => {
-        if (res.status === 200) {
-            setChartData(res.data);
-            setStatus("success");
-        } else {
-            setStatus("error");
-        }
-    });
-}, []);
+    let result;
 
-let result;
+    switch (status) {
+        case 'loading':
+            result = <Loading />;
+            break;
+        case 'success':
+            result = (
+                <MainChartReady
+                    data={chartData}
+                    dateFrom={dateFrom}
+                    dateTo={dateTo}
+                    granularity={granularity}
+                    handleDateFrom={handleDateFrom}
+                    handleDateTo={handleDateTo}
+                    handleGranularity={handleGranularity}
+                    handleReset={reset}
+                    handleSet={set}
+                />
+            );
+            break;
+        case 'error':
+            result = <Error content="Error" />;
+    }
 
-switch (status) {
-    case "loading":
-        result = <Loading/>
-        break;
-    case "success":
-        result = (
-            <MainChartReady 
-                data={chartData}
-                dateFrom={dateFrom}
-                dateTo={dateTo}
-                granularity={granularity}
-                handleDateFrom={handleDateFrom}
-                handleDateTo={handleDateTo}
-                handleGranularity={handleGranularity}
-                handleReset={reset}
-                handleSet={set}
-            />
-        )
-        break;
-    case "error":
-        result = <Error content="Error"/>
-}
-
-return  result;
+    return result;
 };
 
 export default MainChartLoad;
--- dashboard/client/src/components/charts/ChartMainPage.js
+++ dashboard/client/src/components/charts/ChartMainPage.js
@@ -1,95 +1,93 @@
-import React, { useState, useEffect } from "react";
-import { Container } from "@mui/material";
-import ApiClient from "../../api/ApiClient";
+import React, { useState, useEffect } from 'react';
+import { Container } from '@mui/material';
+import ApiClient from '../../api/ApiClient';
 
-import SimpleLineChart from "./SimpleLineChart";
-import ChartControls from "./ChartControls";
+import SimpleLineChart from './SimpleLineChart';
+import ChartControls from './ChartControls';
 
 const Home = () => {
-  const today = new Date();
-  const yesterday = new Date(today);
-  yesterday.setDate(yesterday.getDate() - 1);
-  const defaultGranularity = 1800;
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+    const defaultGranularity = 1800;
 
-  const [dateFrom, setDateFrom] = React.useState(yesterday);
-  const [dateTo, setDateTo] = React.useState(today);
-  const [granularity, setGranularity] = React.useState(defaultGranularity);
-  console.log(dateTo.toISOString())
-  const handleDateFrom = (newDateFrom) => {
-    setDateFrom(newDateFrom);
-  };
+    const [dateFrom, setDateFrom] = React.useState(yesterday);
+    const [dateTo, setDateTo] = React.useState(today);
+    const [granularity, setGranularity] = React.useState(defaultGranularity);
+    console.log(dateTo.toISOString());
+    const handleDateFrom = (newDateFrom) => {
+        setDateFrom(newDateFrom);
+    };
 
-  const handleDateTo = (newDateTo) => {
-    setDateTo(newDateTo);
-  };
+    const handleDateTo = (newDateTo) => {
+        setDateTo(newDateTo);
+    };
 
-  const handleGranularity = (event) => {
-    setGranularity(event.target.value);
-  };
+    const handleGranularity = (event) => {
+        setGranularity(event.target.value);
+    };
 
-  const reset = () => {
-    setDateFrom(yesterday);
-    setDateTo(today);
-    setGranularity(defaultGranularity);
-  };
+    const reset = () => {
+        setDateFrom(yesterday);
+        setDateTo(today);
+        setGranularity(defaultGranularity);
+    };
 
-  const set = () => {
-    //call server
-  };
+    const set = () => {
+        //call server
+    };
 
-  const [chartData, setChartData] = useState();
+    const [chartData, setChartData] = useState();
 
-  useEffect(() => {
-    ApiClient.getWeatherData(dateFrom.toISOString(), dateTo.toISOString()).then((res) => {
-      setChartData(res.data);
-    });
-  }, []);
+    useEffect(() => {
+        ApiClient.getWeatherData(dateFrom, dateTo, granularity).then((res) => {
+            setChartData(res.data);
+        });
+    }, [dateFrom, dateTo, granularity]);
 
-  let data = [];
+    let data = [];
 
-  if (chartData && chartData.length > 0) {
-    chartData.forEach((element) => {
-      const date = new Date(element.timestamp);
-      data.push({
-        name: date.toLocaleString(),
-        temperature: element.temperature,
-        humidity: element.humidity,
-      });
-    });
-  }
+    if (chartData && chartData.length > 0) {
+        chartData.forEach((element) => {
+            const date = new Date(element.timestamp);
+            data.push({
+                name: date.toLocaleString(),
+                temperature: element.temperature,
+                humidity: element.humidity,
+            });
+        });
+    }
 
+    const lines = [
+        {
+            type: 'monotone',
+            dataKey: 'temperature',
+            stroke: '#8884d8',
+        },
+        {
+            type: 'monotone',
+            dataKey: 'humidity',
+            stroke: '#82ca9d',
+        },
+    ];
 
-  const lines = [
-    {
-      type: "monotone",
-      dataKey: "temperature",
-      stroke: "#8884d8",
-    },
-    {
-      type: "monotone",
-      dataKey: "humidity",
-      stroke: "#82ca9d",
-    },
-  ];
-
-  console.log(dateTo);
-  return (
-    <Container style={{ height: "600px", paddingBottom: "20px" }}>
-      <h1 style={{marginTop:"15px"}}>Graf s naměřenými hodnotami</h1>
-      <ChartControls 
-        dateFrom={dateFrom}
-        dateTo={dateTo}
-        granularity={granularity}
-        handleDateFrom={handleDateFrom}
-        handleDateTo={handleDateTo}
-        handleGranularity={handleGranularity}
-        handleReset={reset}
-        handleSet={set}
-      />
-      <SimpleLineChart data={data} lines={lines} />
-    </Container>
-  );
+    console.log(dateTo);
+    return (
+        <Container style={{ height: '600px', paddingBottom: '20px' }}>
+            <h1 style={{ marginTop: '15px' }}>Graf s naměřenými hodnotami</h1>
+            <ChartControls
+                dateFrom={dateFrom}
+                dateTo={dateTo}
+                granularity={granularity}
+                handleDateFrom={handleDateFrom}
+                handleDateTo={handleDateTo}
+                handleGranularity={handleGranularity}
+                handleReset={reset}
+                handleSet={set}
+            />
+            <SimpleLineChart data={data} lines={lines} />
+        </Container>
+    );
 };
 
 export default Home;
-
