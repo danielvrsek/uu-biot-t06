d0db369127ef9230ea7b861f6705563df863e492
Jakub Pelz <jakubpelz@gmail.com>
2022-06-11 15:41:48 +0200

    LoadUserDOne

--- dashboard/client/src/admin_console/components/weatherstation/ListWeatherStationUsers.js
+++ dashboard/client/src/admin_console/components/weatherstation/ListWeatherStationUsers.js
@@ -3,20 +3,35 @@ import { Container, Grid, Typography } from '@mui/material';
 import AddWeatherStationUser from './AddWeatherStationUser';
 import UserWeatherStatuinDetail from './UserWeatherStationDetail';
 import ApiClient from '../../../api/ApiClient';
+import Error from '../core/Error';
+import Loading from '../core/Loading';
 
 const ListWeateherUsers = () => {
     const [users, setUsers] = useState();
-
+    const [detailStatus, setDetailStatus] = useState('loading');
     useEffect(() => {
         ApiClient.getCurrentWorkspaceUsers()
             .then((response) => {
-                console.log(response);
-                setUsers(response);
+                setUsers(response.data);
+                setDetailStatus('success');
             })
             .catch((error) => {
                 return error;
             });
     }, []);
+    let detailResult;
+
+    switch (detailStatus) {
+        case 'success':
+            detailResult = <UserWeatherStatuinDetail data={users} />;
+            break;
+        case 'error':
+            detailResult = <Error content="Error" />;
+            break;
+        default:
+            detailResult = <Loading />;
+    }
+
     return (
         <Container sx={{ pt: 4 }}>
             <Typography variant="h3" mb={3}>
@@ -24,7 +39,7 @@ const ListWeateherUsers = () => {
             </Typography>
             <AddWeatherStationUser />
             <Grid container spacing={2}>
-                <UserWeatherStatuinDetail />
+                {detailResult}
             </Grid>
         </Container>
     );
--- dashboard/client/src/admin_console/components/weatherstation/UserWeatherStationDetail.js
+++ dashboard/client/src/admin_console/components/weatherstation/UserWeatherStationDetail.js
@@ -1,68 +1,71 @@
 import React from 'react';
 
 import {
-  Grid,
-  Card,
-  CardActionArea,
-  Divider,
-  CardHeader,
-  CardContent,
-  IconButton,
-  Popover,
-  MenuList,
-  MenuItem,
+    Grid,
+    Card,
+    CardActionArea,
+    Divider,
+    CardHeader,
+    CardContent,
+    IconButton,
+    Popover,
+    MenuList,
+    MenuItem,
 } from '@mui/material';
 
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const UserWeatherStationDetail = (props) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handleOpenSettings = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleOpenSettings = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const handleCloseSettings = () => {
-    setAnchorEl(null);
-  };
+    const handleCloseSettings = () => {
+        setAnchorEl(null);
+    };
 
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
 
-  return (
-    <Grid item xs={12} sm={6} md={4} lg={3}>
-      <Card>
-        <CardHeader
-          /* title={props.data.name} */
-          action={
-            <div>
-              <IconButton onClick={handleOpenSettings} aria-label="settings">
-                <MoreVertIcon />
-              </IconButton>
-              <Popover
-                id={id}
-                open={open}
-                anchorEl={anchorEl}
-                onClose={handleCloseSettings}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-              >
-                <MenuList>
-                  <MenuItem>Odebrat uživatele</MenuItem>
-                </MenuList>
-              </Popover>
-            </div>
-          }
-        />
-        <Divider />
-        <CardActionArea /* href={`/weatherstations/${props.data.id}`} */>
-          <CardContent>Content</CardContent>
-        </CardActionArea>
-      </Card>
-    </Grid>
-  );
+    return (
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+            <Card>
+                <CardHeader
+                    title={props.data.firstName}
+                    action={
+                        <div>
+                            <IconButton
+                                onClick={handleOpenSettings}
+                                aria-label="settings"
+                            >
+                                <MoreVertIcon />
+                            </IconButton>
+                            <Popover
+                                id={id}
+                                open={open}
+                                anchorEl={anchorEl}
+                                onClose={handleCloseSettings}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                            >
+                                <MenuList>
+                                    <MenuItem>Odebrat uživatele</MenuItem>
+                                </MenuList>
+                            </Popover>
+                        </div>
+                    }
+                />
+                <Divider />
+                <CardActionArea>
+                    <CardContent>Content</CardContent>
+                </CardActionArea>
+            </Card>
+        </Grid>
+    );
 };
 
 export default UserWeatherStationDetail;
